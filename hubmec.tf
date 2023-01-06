72099ea02b8081a221dfda3334ca2053b01aec62
meloservices <117586519+meloservices@users.noreply.github.com>
2023-01-06 07:22:02 -0500

    Update hubmec.tf

--- hubmec.tf
+++ hubmec.tf
@@ -100,24 +100,7 @@ resource "azurerm_virtual_machine" "hub-vm" {
 }
 
 
- resource "azurerm_virtual_machine_extension" "install2" {
-  name                 = "install2"
-  virtual_machine_id   = azurerm_virtual_machine.hub-vm.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-
  
- settings = <<SETTINGS
-    {
-        "fileUris": [
-        "https://raw.githubusercontent.com/omec-project/ngic-rtc/master/install.sh"
-        ],
-        "commandToExecute": "bash install.sh"
-    }
-SETTINGS
-
-}
 
 
 # Virtual Network Gateway
